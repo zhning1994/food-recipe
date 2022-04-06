c6d39050745738fe6b50f569308f4f16bffaeb60
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-06 17:27:04 +0800

    setting up the server connection and mongodb

--- server/server.js
+++ server/server.js
@@ -2,3 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
+const app = express();
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.get('/', (req, res) => res.status(200).send('I love orange'))
+const CONNECTION_URL = 'mongodb+srv://appadmin:6xsHG8IRzmqD7Ehc@cluster0.e1iew.mongodb.net/recipedb?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((err) => console.log(err.message))
+//6xsHG8IRzmqD7Ehc
