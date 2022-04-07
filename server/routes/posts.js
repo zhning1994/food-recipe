d234527bcf33f81b9d39b30009b375c6bb247fe7
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-07 15:09:12 +0800

    create createPosts route and controller setting

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getPosts } from "../controllers/post.js";
+import { getPosts, createPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get('/', getPosts);
-
+router.post('/', createPosts);
 export default router;
