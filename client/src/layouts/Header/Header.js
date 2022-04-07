3900c726101ec6cda23a01b5b39338d9ed8a9c98
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-07 19:25:16 +0800

    initialize the outlook of header

--- client/src/layouts/Header/Header.js
+++ client/src/layouts/Header/Header.js
@@ -1,10 +1,25 @@
 import './styles.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
-        <header>
+        <header className=''>
             <img />
+            <div className=''>
+                <ul className=''>
+                    <li className=''>Home</li>
+                    <li className=''>About</li>
+                    <li className=''>Recipe</li>
+                    <li className=''>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <FontAwesomeIcon size='xl' icon={faMagnifyingGlass} />
+            </div>
+            <div>
+                Sign In
+            </div>
 
         </header>
 
