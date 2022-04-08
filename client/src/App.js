d73ab81f8aa7f003607562e5361d9fba6f0505f1
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-08 22:40:49 +0800

    complete features component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from './layouts/Header/Header';
+import Feature from './layouts/Features/Feature';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Feature />
     </div>
   );
 }
---
+++ client/src/layouts/Features/Feature.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Feature.css';
+
+function Feature() {
+    return (
+        <div>
+            <div className='feature'>
+                <div className='feature__sloganContainer'>
+                    <h3 className='feature__slogan'><span>Share</span> Your Most Favourite Food Recipe At Here &amp; Let's <span>Cook</span></h3>
+                    <p className='feature__detail'>Cost of living drastically increasing nowsaday but this shouldn't stop us for having healthy and fresh meals. Let's us make cheap and nice food ourselves. Share your favourite recipe at here, let us enjoy wonderful and cheap dishes.</p>
+                </div>
+                <div className='feature_btnContainer'>
+                    <div className='feature_btnSubContainer'>
+                        <button className='feature__btn'><span>View Recipe</span> <img src="https://img.icons8.com/stickers/100/000000/lemonade.png" /></button>
+                        <button className='feature__btn'><span>Create Recipe</span><img src="https://img.icons8.com/stickers/100/000000/grapes.png" /></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Feature
