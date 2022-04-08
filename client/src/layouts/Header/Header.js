f67024e30b0ec2a704d712b9cdf8e6d5fe16f525
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-08 13:56:48 +0800

    adjust the size of button and completed header section

--- client/src/layouts/Header/Header.js
+++ client/src/layouts/Header/Header.js
@@ -3,17 +3,18 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../UI/Button.js';
+import logo from '../../assets/logo.png';
 
 function Header() {
     return (
-        <header className=''>
-            <img src='' alt='' />
-            <div className=''>
-                <ul className=''>
-                    <li className=''>Home</li>
-                    <li className=''>About</li>
-                    <li className=''>Recipe</li>
-                    <li className=''>Contact</li>
+        <header className='header'>
+            <img className='header__logo' src={logo} alt='logo' />
+            <div className='header__itemsContainer'>
+                <ul className='header__itemContainer'>
+                    <li className='header__item'>Home</li>
+                    <li className='header__item'>About</li>
+                    <li className='header__item'>Recipe</li>
+                    <li className='header__item'>Contact</li>
                 </ul>
             </div>
             <div>
