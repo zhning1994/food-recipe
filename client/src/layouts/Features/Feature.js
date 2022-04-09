d8c0236362ee639ee100acdc3f151a6dddc18604
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-09 19:34:44 +0800

    add alt attribution to img

--- client/src/layouts/Features/Feature.js
+++ client/src/layouts/Features/Feature.js
@@ -11,8 +11,8 @@ function Feature() {
                 </div>
                 <div className='feature_btnContainer'>
                     <div className='feature_btnSubContainer'>
-                        <button className='feature__btn'><span>View Recipe</span> <img src="https://img.icons8.com/stickers/100/000000/lemonade.png" /></button>
-                        <button className='feature__btn'><span>Create Recipe</span><img src="https://img.icons8.com/stickers/100/000000/grapes.png" /></button>
+                        <button className='feature__btn'><span>View Recipe</span> <img src="https://img.icons8.com/stickers/100/000000/lemonade.png" alt='iconPic' /></button>
+                        <button className='feature__btn'><span>Create Recipe</span><img src="https://img.icons8.com/stickers/100/000000/grapes.png" alt='iconPic' /></button>
                     </div>
                 </div>
             </div>
