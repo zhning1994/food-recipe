b3c15704e18a0836e342147feb9d409e155b2b5b
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-13 23:13:47 +0800

    set value attribute and state which needed to upload

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import burger from '../../assets/form-burgerIcon.png';
+import FileBase from 'react-file-base64';
 import './Form.css';
 function Form() {
     const [postData, setPostData] = useState({
@@ -15,41 +16,41 @@ function Form() {
                 <hr className='form__divideLine' />
                 <label className='form__creator'>
                     <span>Name</span>
-                    <input value={postData.creator} placeholder='Zhen Ning' type='text' name='creator' />
+                    <input value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} placeholder='Zhen Ning' type='text' name='creator' />
                 </label>
                 <label className='form__title'>
                     <span>Dish Name</span>
-                    <input value={postData.title} placeholder='Aglio Olio' type='text' name='title' />
+                    <input value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} placeholder='Aglio Olio' type='text' name='title' />
                 </label>
                 <label className='form__type'>
                     <span>Type</span>
-                    <input placeholder='ITALIAN SPICY PASTA' type='text' name='type' />
+                    <input value={postData.type} onChange={(e) => setPostData({ ...postData, type: e.target.value })} placeholder='ITALIAN SPICY PASTA' type='text' name='type' />
                 </label>
                 <label className='form__description'>
                     <span>Description</span>
-                    <textarea placeholder='Some briefly description of this dish.' rows={8} name='description' />
+                    <textarea value={postData.description} onChange={(e) => setPostData({ ...postData, description: e.target.value })} placeholder='Some briefly description of this dish.' rows={8} name='description' />
                 </label>
                 <div className='form__timeAndServing'>
                     <label className='form__time--prepTime'>
                         <span>Prepare Time (mins)</span>
-                        <input type='number' defaultValue={1} name='prepTime' />
+                        <input value={postData.prepTime} onChange={(e) => setPostData({ ...postData, prepTime: e.target.value })} type='number' name='prepTime' />
                     </label>
                     <label className='form__time--cookTime'>
                         <span>Cook Time (mins)</span>
-                        <input className='' type='number' defaultValue={1} name='cookTime' />
+                        <input value={postData.cookTime} onChange={(e) => setPostData({ ...postData, cookTime: e.target.value })} type='number' name='cookTime' />
                     </label>
                     <label className='form__serving'>
                         <span>Serving</span>
-                        <input className='' type='number' defaultValue='1' name='serving' />
+                        <input value={postData.serving} onChange={(e) => setPostData({ ...postData, serving: e.target.value })} type='number' name='servings' />
                     </label>
                 </div>
                 <label className='form__ingredient'>
                     <span>Ingredients</span>
-                    <input placeholder='1/4 onions' type='text' name='ingredients' />
+                    <input value={postData.serving} onChange={(e) => setPostData({ ...postData, serving: e.target.value })} placeholder='1/4 onions' type='text' name='ingredients' />
                 </label>
                 <label className='form__methods'>
                     <span>Methods</span>
-                    <textarea placeholder='Show us some steps how to make this.' rows={8} name='methods' />
+                    <textarea value={postData.method} onChange={(e) => setPostData({ ...postData, method: e.target.value })} placeholder='Show us some steps how to make this.' rows={8} name='methods' />
                 </label>
                 <label className='form__img'>
                     <span>Food Pics</span>
