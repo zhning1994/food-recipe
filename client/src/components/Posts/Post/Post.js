60834040cfe6ce68aba6e41ebfff39e85bc053d1
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-19 19:33:09 +0800

    complete the recipe card style

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsis, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsis, faThumbsUp, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import './Post.css';
 
 function Post({ post }) {
+    const totalTime = post.prepTime + post.cookTime;
     return (
-        <div>
-            <div>
-                <p>{post.creator}</p>
-                <p>{moment(post.createAt).fromNow()}</p>
+        <div className='post__card'>
+            <img className='post__cardImg' src={post.imgURL} alt='recipe pic' />
+            <hr className='post__cardLine' />
+            <div className='post__titleContainer'>
+                <p className='post__type'>{post.type}</p>
+                <p className='post__title'>{post.title}</p>
+                <p className='post__tags'>{post.tags.map((tag) => `#${tag} `)}</p>
+                {/* <p>{moment(post.createAt).fromNow()}</p> */}
             </div>
-            <div>
-                <button className='' onClick={() => { }}><FontAwesomeIcon size='xl' icon={faEllipsis} /></button>
+            <div className='post__expand'>
+                <button className='post__expandBtn' onClick={() => { }}><FontAwesomeIcon className='post__expandBtnIcon' size='xl' icon={faEllipsis} /></button>
+                {/* <FontAwesomeIcon size='xl' icon={faEllipsis} /> */}
             </div>
-            <div>
-                <p>{post.tags.map((tag) => `#${tag} `)}</p>
+            <div className='post__info'>
+                <p className='post__time'>
+                    <span className='post__time--number'>{totalTime}</span>
+                    <span>Times Needed(mins)</span>
+                </p>
+                <p className='post__serving'>
+                    <span className='post__serving--number'>{post.servings}</span>
+                    <span>Serving(s)</span>
+                </p>
             </div>
-            <div>
-                <button><span>Like</span><FontAwesomeIcon icon={faThumbsUp} />{post.likeCount}</button>
-                <button><span>Delete</span><FontAwesomeIcon icon={faThumbsUp} /></button>
+            <div className='post__bot'>
+                <button className='post__like'>
+                    <FontAwesomeIcon className='post__likeBtn' icon={faThumbsUp} />
+                    <span>Like</span>
+                    {post.likeCount}
+                </button>
+                <button className='post__delete'>
+                    <FontAwesomeIcon className='post__deleteBtn' icon={faTrashCan} />
+                    <span>Delete</span>
+                </button>
             </div>
         </div>
     )
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Post from './Post/Post.js';
 import { useSelector } from 'react-redux';
+import './Posts.css';
 
 function Posts() {
     const posts = useSelector((state) => state.posts);
-    console.log(posts);
     return (
-        <div>
+        <div className='post__cardContainer'>
+            <h3 className='posts__recipeHead'>Don't Miss Out This Hot Recipe Hits</h3>
             {posts.map((post => (
                 <div key={post._id}>
                     <Post post={post} />
