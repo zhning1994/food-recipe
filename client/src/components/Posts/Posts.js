9c990d5d0a551d1132f1a93b2ece05ed20f6f8d5
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-19 23:58:40 +0800

    set the media query for displaying recipe on smaller screen

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,14 +6,17 @@ import './Posts.css';
 function Posts() {
     const posts = useSelector((state) => state.posts);
     return (
-        <div className='post__cardContainer'>
+        <>
             <h3 className='posts__recipeHead'>Don't Miss Out This Hot Recipe Hits</h3>
-            {posts.map((post => (
-                <div key={post._id}>
-                    <Post post={post} />
-                </div>
-            )))}
-        </div>
+            <div className='post__cardContainer'>
+                {posts.map((post => (
+                    <div key={post._id}>
+                        <Post post={post} />
+                    </div>
+                )))}
+            </div>
+        </>
+
     )
 }
 
