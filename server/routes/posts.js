7570e32c83a77e0361d122cab30fa4362a57e5fe
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-20 14:25:23 +0800

    setting updatePost route via findByIdAndUpdate in mongoose

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts, createPosts } from "../controllers/post.js";
+import { getPosts, createPosts, updatePost } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPosts);
+router.patch('/:id', updatePost);
 export default router;
