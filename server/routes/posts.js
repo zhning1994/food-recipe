075ac7a7bb26d162e3b6891fedb53882873a9e55
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-21 23:23:00 +0800

    setup the delete route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getPosts, createPosts, updatePost } from "../controllers/post.js";
+import { getPosts, createPosts, updatePost, deletePost } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPosts);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 export default router;
