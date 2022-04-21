fa327b73e100dc82473adddd5f0edef1c7e31235
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-21 18:29:38 +0800

    ObjectId.isValid(__id) should be only one underscore _id

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -28,9 +28,9 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(__id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
-    res.json({ updatedPost });
+    res.json(updatedPost);
 }
