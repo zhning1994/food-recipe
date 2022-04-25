994e7ed816f943ad626b8fbd9f2dfc93f077a617
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-25 22:50:31 +0800

    for auth reducer, localStorage setItem of login and return action data

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,9 @@
+export default (state = { authData: null }, action) => {
+    switch (action.type) {
+        case 'AUTH':
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
+            return { ...state, authData: action?.data };
+        default:
+            return state;
+    }
+}
