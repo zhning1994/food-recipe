325b9c4a122112d72880f9888631cf8048da5150
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-26 14:45:46 +0800

    add the avatar, sign in and log out component to page

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import "./Navbar.css";
+import Avatar from 'react-avatar';
+import Button from '../../UI/Button.js';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+
+
+function Navbar() {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
+
+    useEffect(() => {
+        const token = user?.token;
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location])
+
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+        history.push('/');
+        setUser(null);
+    }
+
+    return (
+        <>
+            {user?.result ? (
+                <div className='navbar__container'>
+                    <div className='navbar__avatar'>
+                        <Avatar className='navbar__subavatar' color='#219ebc' style={{ fontWeight: "bold" }} size="40" round={true} name={user?.result.name.charAt(0)} googleId={user?.result.googleId} alt={user?.result.name} />
+                        {/* <img className='navbar__avatar' src={user?.result.imageUrl} alt={user?.result.name} /> */}
+                        <h6 className='navbar__name'>{user?.result.name}</h6>
+                    </div>
+                    <button className='navbar__btn' onClick={logout}>Logout</button>
+
+                </div>
+            ) : (<div>
+                <Link to='/auth'>
+                    <button className='navbar__btn'>Sign In</button>
+                </Link>
+            </div>)}
+        </>
+    )
+}
+
+export default Navbar;
